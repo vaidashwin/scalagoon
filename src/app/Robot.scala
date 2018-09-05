1728dc06c48e3b16fd1d042a900824a6674c306c
TenjouUtena <brendan.speer@gmail.com>
2018-09-05 13:49:04 -0500

    * Take out test Module * Actually fix URL issue

--- src/app/Robot.scala
+++ src/app/Robot.scala
@@ -20,7 +20,7 @@ object Robot extends App {
   val xpathFactory = XPathFactory.newInstance()
 
   override def main(args: Array[String]): Unit = {
-    val nick, login = "tenjouthief"
+    val nick, login = "gangsinesjr"
     val channel = "#mtgoontest"
     val endLine = "\r\n"
 
@@ -45,13 +45,14 @@ object Robot extends App {
       new GonnaGiveItToYaModule ::
       new ScryfallModule(respond) ::
       new MerriamWebsterModule(respond) ::
-      new ExternModule(new TestModuleClj) ::
+//      Clojure modules are added like so:
+//      new ExternModule(new TestModuleClj) ::
       Nil
 
     val helpFunc: PartialFunction[IrcMessage, Option[IrcMessage]] = {
       case ChatMessage(user, ch, s) if s == s"hi $nick" =>
         val moduleMessage = modules.flatMap(_.helpBlurb).mkString("; ")
-        Some(ChatMessage(nick, ch, s"hi i'm $nick, you can say i'm broken or ask me at https://github.com/vaidashwin/scalagoon. my modules are: $moduleMessage"))
+        Some(ChatMessage(nick, ch, s"hi i'm $nick, you can say i'm broken or ask me at https://github.com/vaidashwin/scalagoon/  My modules are: $moduleMessage"))
     }
 
     val moduleFunc: PartialFunction[IrcMessage, Option[IrcMessage]] = helpFunc orElse modules.map(_.func).reduce(_ orElse _)
